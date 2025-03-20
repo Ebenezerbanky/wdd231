ad14842f4cd48c0dd7e1a417ab8efec2a161dd08
Ebenezerbanky <bankoleebenezer111@gmail.com>
2025-03-20 05:26:51 +0100

    add homepage to wdd231

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+
+        // Display the current year
+        document.getElementById('year').textContent = new Date().getFullYear();
+
+        // Display the last modified date
+        document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+
 function filterCourses(category) {
     const courses = document.querySelectorAll('.course-item');
     courses.forEach(course => {
